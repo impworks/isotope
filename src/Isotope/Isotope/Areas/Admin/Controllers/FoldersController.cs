bf54b9f678e18670a97865a24e0af502367a541c
Andrei Kurosh <mail@spark.ms>
2020-10-12 11:10:57 +0300

    FoldersController.

---
+++ src/Isotope/Isotope/Areas/Admin/Controllers/FoldersController.cs
@@ -0,0 +1,68 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Isotope.Areas.Admin.Dto;
+using Isotope.Areas.Admin.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Isotope.Areas.Admin.Controllers
+{
+    /// <summary>
+    /// Controller for working with folders.
+    /// </summary>
+    [Route("~/@api/admin/folders")]
+    public class FoldersController: AdminControllerBase
+    {
+        public FoldersController(FolderManager folderMgr)
+        {
+            _folderMgr = folderMgr;
+        }
+        
+        private readonly FolderManager _folderMgr;
+
+        /// <summary>
+        /// Returns the tree of all folders in the system.
+        /// </summary>
+        [HttpGet, Route("")]
+        public Task<IReadOnlyList<FolderTitleVM>> GetTree()
+        {
+            return _folderMgr.GetTreeAsync();
+        }
+
+        /// <summary>
+        /// Creates a new folder.
+        /// </summary>
+        [HttpPost, Route("{key}")]
+        public Task<FolderTitleVM> Create(string key, [FromBody] FolderVM vm)
+        {
+            return _folderMgr.CreateAsync(key, vm);
+        }
+
+        /// <summary>
+        /// Returns the folder info for editing.
+        /// </summary>
+        [HttpGet, Route("{key}")]
+        public Task<FolderVM> GetDetails(string key)
+        {
+            return _folderMgr.GetAsync(key);
+        }
+
+        /// <summary>
+        /// Updates an existing folder.
+        /// </summary>
+        [HttpPut, Route("{key}")]
+        public Task Update(string key, FolderVM vm)
+        {
+            vm.Key = key;
+            return _folderMgr.UpdateAsync(vm);
+        }
+
+        /// <summary>
+        /// Removes an existing folder with all its contents.
+        /// </summary>
+        [HttpDelete, Route("{key}")]
+        public Task Delete(string key)
+        {
+            return _folderMgr.RemoveAsync(key);
+        }
+    }
+}
