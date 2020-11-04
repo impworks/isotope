50ae7c2f3e3935724138548c849e5bef468be612
Andrei Kurosh <mail@spark.ms>
2020-11-04 20:12:15 +0300

    Admin menu styles: icons, cursor, etc.

--- src/Isotope/Isotope/Areas/Admin/ClientApp/source/components/utils/MainMenu.vue
+++ src/Isotope/Isotope/Areas/Admin/ClientApp/source/components/utils/MainMenu.vue
@@ -9,15 +9,40 @@ export default class MainMenu extends Vue {
 <template>
     <div>
         <ul class="list-group mb-4">
-            <router-link to="/folders" tag="li" class="list-group-item clickable">Content</router-link>
-            <router-link to="/tags" tag="li" class="list-group-item clickable">Tags</router-link>
+            <router-link to="/folders" tag="li" class="list-group-item clickable">
+                <span class="fa fa-fw fa-folder-open-o"></span>
+                Content
+            </router-link>
+            <router-link to="/tags" tag="li" class="list-group-item clickable">
+                <span class="fa fa-fw fa-tags"></span>
+                Tags
+            </router-link>
         </ul>
         <ul class="list-group mb-4">
-            <router-link to="/shared-links" tag="li" class="list-group-item clickable">Shared links</router-link>
+            <router-link to="/shared-links" tag="li" class="list-group-item clickable">
+                <span class="fa fa-fw fa-share-square"></span>
+                Shared links
+            </router-link>
         </ul>
         <ul class="list-group">
-            <router-link to="/users" tag="li" class="list-group-item clickable">Users</router-link>
-            <router-link to="/config" tag="li" class="list-group-item clickable">Config</router-link>
+            <router-link to="/users" tag="li" class="list-group-item clickable">
+                <span class="fa fa-fw fa-user"></span>
+                Users
+            </router-link>
+            <router-link to="/config" tag="li" class="list-group-item clickable">
+                <span class="fa fa-fw fa-wrench"></span>
+                Config
+            </router-link>
         </ul>
     </div>
 </template>
+
+<style lang="scss" scoped>
+.list-group-item {
+    padding-left: 0.75rem;
+    
+    .fa {
+        margin-right: 0.25rem;
+    }
+}
+</style>
